fa7b9c8a0bec05d9362e063c7cd3a541d83ab632
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-05-02 19:34:38 -0300

    feat: (auth) add slack and prisma

--- auth.ts
+++ auth.ts
@@ -1,9 +1,23 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import Slack from "next-auth/providers/slack"
 import prisma from "@/services/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [Google,
+    Slack({
+      clientId: process.env.AUTH_SLACK_ID!,
+      clientSecret: process.env.AUTH_SLACK_SECRET!,
+      authorization: {
+        params: { scope: "users:read.email" },
+      },
+    }),
+  ],
   callbacks: {
     async signIn({ user, email }) {
       const userEmail = email ?? user.email;
@@ -11,20 +25,33 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     
       const domain = userEmail.split("@")[1].toLowerCase();
     
-      const company = await prisma.company.findFirst({
-        where: {
-          emailDomains: {
-            has: domain,
-          },
-        },
+      const validDomain = await prisma.emailDomains.findUnique({
+        where: { email: domain },
       });
     
-      if (!company) {
+      if (!validDomain) {
         console.log("Blocked sign-in for unrecognized email domain:", domain);
         return false;
       }
     
       return true;
     }
-  },    
+  },
+
+  events: {
+    // dispara logo depois que o usuário é criado no banco
+    async createUser({ user }) {
+      const domain = user.email!.split("@")[1].toLowerCase();
+      const alias = await prisma.emailDomains.findUnique({
+        where: { email: domain },
+      });
+      if (!alias) {
+        throw new Error("Domínio de e-mail não autorizado.");
+      }
+      await prisma.user.update({
+        where: { id: user.id },
+        data: { companyId: alias.companyId },
+      });
+    },
+  },
 })
